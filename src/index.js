5426cba45c62918b578f37986de85c9c6807d0d2
Zalya <pzalivako@mail.ru>
2019-10-13 22:57:03 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let splitArr = str.split('');
+  let bool;
+  let bracketsConfigLength = bracketsConfig.length;
+  for (let j = 0; j < bracketsConfigLength; j++){
+    for (let i = 0; i < splitArr.length;){
+      if (splitArr[i] == bracketsConfig[j][0] && splitArr[i + 1] == bracketsConfig[j][1]){
+        splitArr.splice(i, 2);
+        i = 0;
+        j = 0;
+      }
+      else {
+        i++;
+      }    
+    }
+  }
+  if (splitArr.length == 0 ) {
+    bool = true;
+  } 
+  else {
+    bool = false;
+  }
+  return bool;
 }
